5ae0242986034d93f5e0ac4652ef4717ba35f1ce
mobliano <kyle@mobley.org>
2023-09-10 21:42:11 -0700

    Setup 4.0.5 and make some initial config changes

---
+++ whimsyhell/1.19.2/4.0.5/kubejs/startup_scripts/custom_items.js
@@ -0,0 +1,37 @@
+// priority: 0
+
+console.info('KubeJS - Loading Whimsyhell Custom Items')
+
+// Custom item registry
+StartupEvents.registry('item', event => {
+    // Textures for these items are located at kubejs/assets/whimsyhell/textures/item/
+    // event.create('whimsyhell:whimsy_crystal').displayName('§bWhimsy Crystal')
+    event.create('whimsyhell:whimsy_dust').displayName('§bWhimsy Dust')
+    event.create('whimsyhell:whimsy_ingot').displayName('§bWhimsy Ingot')
+    // event.create('whimsyhell:whimsy_nugget').displayName('§bWhimsy Nugget')
+    // event.create('whimsyhell:whimsy_plate').displayName('§bWhimsy Plate')
+
+    event.create('whimsyhell:whimsy_sword', 'sword')
+    .tier('netherite')
+    .attackDamageBaseline(45.0)
+    .speedBaseline(5.0)
+    .maxDamage(8000)
+    .tooltip("§5Enemies hear a whimsical slice!")  
+    .tooltip("§5…and then silence forever.")
+    .displayName("The Sword of Whimsyhell")
+
+    event.create('whimsyhell:whimsy_sword_dev', 'sword')
+    .tier('netherite')
+    .attackDamageBaseline(999999.0)
+    .speedBaseline(5.0)
+    .maxDamage(8000)
+    .tooltip("§5Sword for development use only.")  
+    .displayName("The Sword of Whimsyhell Development")
+})
+
+
+// Custom block registry
+// StartupEvents.registry('block', event => {
+// 	// Textures for these items are located at kubejs/assets/whimsyhell/textures/block/
+//     // Custom block models are located at kubejs/assets/whimsyhell/models/block/
+// })
