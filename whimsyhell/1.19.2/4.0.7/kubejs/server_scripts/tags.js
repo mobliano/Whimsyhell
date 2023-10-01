33fbaed1d65cecde41b192c8908cdbdb83869965
mobliano <kyle@mobley.org>
2023-10-01 13:34:47 -0700

    Whimsyhell 4.0.7 Changes

---
+++ whimsyhell/1.19.2/4.0.7/kubejs/server_scripts/tags.js
@@ -0,0 +1,9 @@
+// priority: 0
+
+console.info('KubeJS - Loading Whimsyhell Custom Tags')
+
+ServerEvents.tags('item', event => {
+    // Whimsy Ingot and Whimsy Plate tag used to crush back to Whimsy Dust
+    // event.add('whimsyhell:whimsy_ingot_plate_tag', 'whimsyhell:whimsy_ingot')
+    // event.add('whimsyhell:whimsy_ingot_plate_tag', 'whimsyhell:whimsy_plate')
+})
